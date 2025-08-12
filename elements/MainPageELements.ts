b4815af04437aebf5d6a823eb0908affc0bcc5e4
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2025-08-12 23:57:31 +0300

    Add text prop to elements array

--- elements/MainPageELements.ts
+++ elements/MainPageELements.ts
@@ -2,37 +2,46 @@ export const elements = [
   {
     locator: (page) => page.getByRole('link', { name: 'Playwright logo Playwright' }),
     name: 'Playwright logo Playwright',
+    text: 'Playwright',
   },
   {
     locator: (page) => page.getByRole('link', { name: 'Docs' }),
     name: 'Docs',
+    text: 'Docs',
   },
   {
     locator: (page) => page.getByRole('link', { name: 'API' }),
     name: 'API',
+    text: 'API',
   },
   {
     locator: (page) => page.getByRole('button', { name: 'Node.js' }),
     name: 'Node.js',
+    text: 'Node.js',
   },
   {
     locator: (page) => page.getByRole('link', { name: 'Community' }),
     name: 'Community',
+    text: 'Community',
   },
   {
     locator: (page) => page.getByRole('link', { name: 'GitHub repository' }),
     name: 'GitHub repository',
+    text: '',
   },
   {
     locator: (page) => page.getByRole('link', { name: 'Discord server' }),
     name: 'Discord server',
+    text: '',
   },
   {
     locator: (page) => page.getByRole('button', { name: 'Switch between dark and light' }),
     name: 'Switch between dark and light',
+    text: '',
   },
   {
     locator: (page) => page.getByRole('button', { name: 'Search (Command+K)' }),
     name: 'Search (Command+K)',
+    text: '',
   },
 ];
