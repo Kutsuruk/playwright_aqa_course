c4b392097323c50d82cb9d051f6972ec3309be97
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2025-08-13 00:23:09 +0300

    Add attributes object to collect data for href values

--- elements/MainPageELements.ts
+++ elements/MainPageELements.ts
@@ -3,16 +3,28 @@ export const elements = [
     locator: (page) => page.getByRole('link', { name: 'Playwright logo Playwright' }),
     name: 'Playwright logo Playwright',
     text: 'Playwright',
+    attributes: {
+      type: 'href',
+      value: '/',
+    },
   },
   {
     locator: (page) => page.getByRole('link', { name: 'Docs' }),
     name: 'Docs',
     text: 'Docs',
+    attributes: {
+      type: 'href',
+      value: '/docs/intro',
+    },
   },
   {
     locator: (page) => page.getByRole('link', { name: 'API' }),
     name: 'API',
     text: 'API',
+    attributes: {
+      type: 'href',
+      value: '/docs/api/class-playwright',
+    },
   },
   {
     locator: (page) => page.getByRole('button', { name: 'Node.js' }),
@@ -23,16 +35,28 @@ export const elements = [
     locator: (page) => page.getByRole('link', { name: 'Community' }),
     name: 'Community',
     text: 'Community',
+    attributes: {
+      type: 'href',
+      value: '/community/welcome',
+    },
   },
   {
     locator: (page) => page.getByRole('link', { name: 'GitHub repository' }),
     name: 'GitHub repository',
     text: '',
+    attributes: {
+      type: 'href',
+      value: 'https://github.com/microsoft/playwright',
+    },
   },
   {
     locator: (page) => page.getByRole('link', { name: 'Discord server' }),
     name: 'Discord server',
     text: '',
+    attributes: {
+      type: 'href',
+      value: 'https://aka.ms/playwright/discord',
+    },
   },
   {
     locator: (page) => page.getByRole('button', { name: 'Switch between dark and light' }),
