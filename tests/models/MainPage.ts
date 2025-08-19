006f10b58dd8f4d5997efc3fcc9574cac8a3b779
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2025-08-20 01:47:25 +0300

    Move all tests from mainPage spec to separate methods of MainPage class

--- tests/models/MainPage.ts
+++ tests/models/MainPage.ts
@@ -1,14 +1,16 @@
 import test, { expect, Page } from '@playwright/test';
-import { elements } from '../../elements/MainPageElements';
-import { Elements } from '../../Interfaces/Elements.interface';
+import { elements, lightModes } from '../../elements/MainPageElements';
+import { Elements, LightModes } from '../../Interfaces/Elements.interface';
 
 export class MainPage {
   readonly page: Page;
   readonly elements: Elements[];
+  readonly lightModes: LightModes[];
 
   constructor(page: Page) {
     this.page = page;
     this.elements = elements;
+    this.lightModes = lightModes;
   }
 
   async openMainPage() {
@@ -20,6 +22,63 @@ export class MainPage {
       test.step(`Check the availability of header's ${name} logos`, async () => {
         await expect.soft(locator(this.page)).toBeVisible();
       });
+    }
+  }
+
+  async checkHeaderElementsNaming() {
+    for (const { locator, name, text } of this.elements) {
+      if (text) {
+        test.step(`Check the text of header's ${name} element`, async () => {
+          await expect.soft(locator(this.page)).toContainText(text);
+        });
+      }
+    }
+  }
+
+  async checkHeaderElementHrefAttribute() {
+    for (const { locator, name, attributes } of this.elements) {
+      if (attributes) {
+        test.step(`Check the href of ${name} header element`, async () => {
+          await expect(locator(this.page)).toHaveAttribute(attributes?.type, attributes?.value);
+        });
+      }
+    }
+  }
+
+  async clickSwitchThemeButton() {
+    await this.page.getByLabel('Switch between dark and light').click();
+    await expect(this.page.locator('html')).toHaveAttribute('data-theme', 'light');
+  }
+
+  async checkHeaderElementHeadingTitle() {
+    for (const { locator, name } of this.elements) {
+      async () => {
+        await expect(locator(this.page)).toBeVisible();
+        await expect(locator(this.page)).toContainText(name);
+      };
+    }
+  }
+
+  async checkGetStartedButtonAvailability() {
+    for (const { locator, name, attributes } of this.elements) {
+      async () => {
+        if (attributes) {
+          await expect(locator(this.page)).toBeVisible();
+          await expect(locator(this.page)).toContainText(name);
+          await expect(locator(this.page)).toHaveAttribute(attributes?.value, attributes?.type);
+        }
+      };
+    }
+  }
+
+  async checkTheActiveThemeStatus() {
+    for (const value of this.lightModes) {
+      async ({ page }) => {
+        await page.evaluate((value) => {
+          document.querySelector('html')?.setAttribute('data-theme', value);
+        }, value);
+        await expect(page).toHaveScreenshot(`page-with-${value}.png`);
+      };
     }
   }
 }
--- tests/pages/mainPage.spec.ts
+++ tests/pages/mainPage.spec.ts
@@ -3,10 +3,6 @@ import { elements, lightModes } from '../../elements/MainPageElements';
 import { MainPage } from '../models/MainPage';
 
 test.describe('Main page test', () => {
-  test.beforeEach(async ({ page }) => {
-    // await page.goto('https://playwright.dev/');
-  });
-
   test('Check exist of header nav elements', async ({ page }) => {
     const mainPage = new MainPage(page);
     await mainPage.openMainPage();
@@ -14,57 +10,38 @@ test.describe('Main page test', () => {
   });
 
   test('Check that header nav elems has correct naming', async ({ page }) => {
-    elements.forEach(({ locator, name, text }) => {
-      if (text) {
-        test.step(`Check the text of header's ${name} element`, async () => {
-          await expect.soft(locator(page)).toContainText(text);
-        });
-      }
-    });
+    const mainPage = new MainPage(page);
+    await mainPage.openMainPage();
+    await mainPage.checkHeaderElementsNaming();
   });
 
   test('Check the href attribute of headers link', async ({ page }) => {
-    elements.forEach(({ locator, name, attributes }) => {
-      if (attributes) {
-        test.step(`Check the href of ${name} header element`, async () => {
-          await expect(locator(page)).toHaveAttribute(attributes?.type, attributes?.value);
-        });
-      }
-    });
+    const mainPage = new MainPage(page);
+    await mainPage.openMainPage();
+    await mainPage.checkHeaderElementHrefAttribute();
   });
 
   test('Check the changing of theme mode', async ({ page }) => {
-    await page.getByLabel('Switch between dark and light').click();
-    await expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
+    const mainPage = new MainPage(page);
+    await mainPage.openMainPage();
+    await mainPage.clickSwitchThemeButton();
   });
 
   test('Check the h1 title', async ({ page }) => {
-    elements.forEach(({ locator, name }) => {
-      async () => {
-        await expect(locator(page)).toBeVisible();
-        await expect(locator(page)).toContainText(name);
-      };
-    });
+    const mainPage = new MainPage(page);
+    await mainPage.openMainPage();
+    await mainPage.checkHeaderElementHeadingTitle();
   });
 
   test('Get started button is present on the page', async ({ page }) => {
-    elements.forEach(({ locator, name, attributes }) => {
-      async () => {
-        if (attributes) {
-          await expect(locator(page)).toBeVisible();
-          await expect(locator(page)).toContainText(name);
-          await expect(locator(page)).toHaveAttribute(attributes?.value, attributes?.type);
-        }
-      };
-    });
+    const mainPage = new MainPage(page);
+    await mainPage.openMainPage();
+    await mainPage.checkGetStartedButtonAvailability();
   });
 
-  lightModes.forEach((value) => {
-    test(`Check the active style of ${value} mode`, async ({ page }) => {
-      await page.evaluate((value) => {
-        document.querySelector('html')?.setAttribute('data-theme', value);
-      }, value);
-      await expect(page).toHaveScreenshot(`page-with-${value}.png`);
-    });
+  test(`Check the active style of theme mode`, async ({ page }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.openMainPage();
+    mainPage.checkTheActiveThemeStatus();
   });
 });
