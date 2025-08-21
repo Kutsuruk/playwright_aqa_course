7c738de91c50b4ec0017d40062ed0ced53a87c21
Vitalii Kutsuruk <kutsurukvet@gmail.com>
2025-08-21 21:34:06 +0300

    Create a fixture for mainPage. Made some refactoring

--- tests/pages/mainPage.spec.ts
+++ tests/pages/mainPage.spec.ts
@@ -1,45 +1,37 @@
-import { test } from '@playwright/test';
-import { MainPage } from '../models/MainPage';
+import { test, expect } from '../fixtures/mainPage';
 
 test.describe('Main page test', () => {
-  let mainPage: MainPage;
-
-  test.beforeEach(async ({ page }) => {
-    mainPage = new MainPage(page);
-    await mainPage.openMainPage();
-  });
-
-  test('Check exist of header nav elements', async ({}) => {
+  test('Check exist of header nav elements', async ({ mainPage }) => {
     await mainPage.checkHeaderElementsVisability();
   });
 
-  test('Check that header nav elems has correct naming', async ({}) => {
+  test('Check that header nav elems has correct naming', async ({ mainPage }) => {
     await mainPage.openMainPage();
     await mainPage.checkHeaderElementsNaming();
   });
 
-  test('Check the href attribute of headers link', async ({}) => {
+  test('Check the href attribute of headers link', async ({ mainPage }) => {
     await mainPage.openMainPage();
     await mainPage.checkHeaderElementHrefAttribute();
   });
 
-  test('Check the changing of theme mode', async ({}) => {
+  test('Check the changing of theme mode', async ({ mainPage }) => {
     await mainPage.openMainPage();
     await mainPage.clickSwitchThemeIcon();
     await mainPage.checkDataThemeAttributeValue();
   });
 
-  test('Check the h1 title', async ({}) => {
+  test('Check the h1 title', async ({ mainPage }) => {
     await mainPage.openMainPage();
     await mainPage.checkHeaderElementHeadingTitle();
   });
 
-  test('Get started button is present on the page', async ({}) => {
+  test('Get started button is present on the page', async ({ mainPage }) => {
     await mainPage.openMainPage();
     await mainPage.checkGetStartedButtonAvailability();
   });
 
-  test(`Check the active style of theme mode`, async ({}) => {
+  test(`Check the active style of theme mode`, async ({ mainPage }) => {
     await mainPage.openMainPage();
     mainPage.checkTheActiveThemeStatus();
   });
